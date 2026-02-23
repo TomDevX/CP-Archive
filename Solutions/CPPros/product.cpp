f092afbcc0d4ba5377b8b5cf91a4ef540186ae16
TomDevX <hoangquan0111@gmail.com>
2026-02-23 09:53:44 +0700

    test PTNK

---
+++ Solutions/CPPros/product.cpp
@@ -0,0 +1,65 @@
+/**
+ *    author: TomDev - Tran Hoang Quan
+ *    created: 2026-02-23 09:14:18
+ *    country: Vietnam - VNM
+ * ----------------------------------------------------------
+ *    title: product
+ *    source: https://oj.vnoi.info/problem/product
+ *    submission: 
+ *    status: WIP
+ * ----------------------------------------------------------
+ *    tags: 
+ *    complexity: 
+ *    note: 
+**/
+
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <utility>
+#if __has_include("debuggingz.h")
+    #include "debuggingz.h"
+    #define dbg(x,i) cerr << "BreakPoint(" << i << ") -> " << #x << " = " << (x) << '\n';
+#else
+    #define dbg(x,i)
+#endif
+
+using namespace std;
+
+#define all(x,bonus) (x).begin()+(bonus),(x).end()
+#define rall(x,bonus) (x).rbegin(),(x).rend()-(bonus)
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fi first
+#define se second
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+using ll = long long;
+using ld = long double;
+using pll = pair<long long,long long>;
+using pld = pair<long double,long double>;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<long long>;
+using vvll = vector<vector<long long>>;
+
+void setup(){
+    if(!fopen("NAME.INP", "r")) return;
+    freopen("NAME.INP", "r", stdin);
+    freopen("NAME.OUT", "w", stdout);
+}
+
+// ----------------------- [ CONFIG & CONSTANTS ] -----------------------
+
+
+// ----------------------- [ FUNCTIONS ] -----------------------
+
+
+// ----------------------- [ MAIN ] -----------------------
+int main(){
+    fastio;
+    setup();
+
+    
+}
