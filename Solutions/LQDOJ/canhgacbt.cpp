a3a036690af7706ba005bb074b26d54daf7885af
TomDevX <hoangquan0111@gmail.com>
2026-04-04 00:36:52 +0700

    add LQDOJ canhgacbt Canh gác bảo tàng

--- Solutions/LQDOJ/canhgacbt.cpp
+++ Solutions/LQDOJ/canhgacbt.cpp
@@ -1,16 +1,16 @@
 /**
  *    author: TomDev - Tran Hoang Quan
- *    created: 2026-04-03 10:57:53
+ *    created: 2026-04-04 00:13:33
  *    country: Vietnam - VNM
  * ----------------------------------------------------------
- *    title: 
- *    source: 
- *    submission: 
- *    status: WIP
+ *    title: Canh gác bảo tàng
+ *    source: https://lqdoj.edu.vn/problem/canhgacbt
+ *    submission: https://lqdoj.edu.vn/submission/8343061
+ *    status: AC
  * ----------------------------------------------------------
- *    tags: 
- *    complexity: 
- *    note: 
+ *    tags: Segment Tree, Dynamic Programming
+ *    complexity: O(n \log n)
+ *    note: Beware that the problem statement is kinda misunderstand because if the ith guard fill in [st;ed] so ed is not occupied yet, and ed maybe > n. Let dp[i] = min cost to fill all the gap in [0;a[i].ed] and we must choose guard i, in order to not let any gap, we have to let dp[i] = min(dp[st -> ed-1]) + a[i].c. For brute force, we can iterate another pointer to get the min value of dp in that range. But what solves a range problem better than segment tree? So we use segment tree to optimize this.
 **/
 
 #include <iostream>
@@ -59,9 +59,9 @@ using vpill = vector<pair<int,long long>>;
 using vpll = vector<pair<long long,long long>>;
 
 void setup(){
-    if(!fopen("main.INP", "r")) return;
-    freopen("main.INP", "r", stdin);
-    freopen("main.OUT", "w", stdout);
+    if(!fopen("GALLERY.INP", "r")) return;
+    freopen("GALLERY.INP", "r", stdin);
+    freopen("GALLERY.OUT", "w", stdout);
 }
 
 // ----------------------- [ CONFIG & CONSTANTS ] -----------------------
@@ -74,13 +74,12 @@ struct Guard{
     }
 };
 const int N = 1e5;
-ll lim;
 ll st[4*N];
 
 // ----------------------- [ FUNCTIONS ] -----------------------
-void update(int id, int l, int  r, int pos, ll val){
+void update(int id, int l, int r, int pos, ll val){
     if(l == r){
-        st[id] = min(st[id] , val);
+        st[id] = min(st[id], val);
         return;
     }
 
@@ -90,11 +89,11 @@ void update(int id, int l, int  r, int pos, ll val){
     if(pos <= mid) update(lc,l,mid,pos,val);
     else update(lc|1,mid+1,r,pos,val);
 
-    st[id] = min(st[lc], st[lc|1]);
+    st[id] = min(st[lc], st[lc | 1]);
 }
 
 ll get(int id, int l, int r, int u, int v){
-    if(l > v || r < u) return lim;
+    if(l > v || r < u) return 2e18;
     if(l >= u && r <= v) return st[id];
 
     int mid = l + ((r-l)>>1);
@@ -108,18 +107,17 @@ int main(){
     fastio;
     setup();
     memset(st,0x3f,sizeof(st));
-    lim = st[0];
     
     int n,m;
     cin >> n >> m;
+
     vector<Guard> a(m+1);
-    for(int i = 1; i <= m; i++) cin >> a[i].st >> a[i].ed >> a[i].c;
-    
+    for(int i = 1; i <= m; i++){
+        cin >> a[i].st >> a[i].ed >> a[i].c;
+    }
     sort(all(a,1));
 
-    int cur = 1;
-
-    vll dp(m+1, lim);
+    vll dp(m+1,2e18);
     for(int i = 1; i <= m; i++){
         if(a[i].st == 0){
             dp[i] = a[i].c;
@@ -128,15 +126,17 @@ int main(){
     }
 
     for(int i = 1; i <= m; i++){
-        ll val = get(1,0,n,a[i].st,a[i].ed);
+        ll val = get(1,0,n,a[i].st,a[i].ed-1);
         dp[i] = val + a[i].c;
-        update(1,0,n,a[i].ed, dp[i]);
+        update(1,0,n,a[i].ed,dp[i]);
     }
+
 
     ll ans = 2e18;
     for(int i = 1; i <= m; i++){
-        if(a[i].ed == n) ans = min(ans, dp[i]);
+        if(a[i].ed >= n) ans = min(ans, dp[i]);
     }
+
     cout << ans;
     
     return NAH_I_WOULD_WIN;
