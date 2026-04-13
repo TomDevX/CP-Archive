5fe1970c9045fe9ac9f8661393ed31ffef9d0755
TomDevX <hoangquan0111@gmail.com>
2026-04-14 00:14:05 +0700

    update from mac

--- Solutions/main.cpp
+++ Solutions/main.cpp
@@ -1,10 +1,94 @@
+/**
+ *    author: TomDev - Tran Hoang Quan
+ *    created: 2026-04-12 00:44:28
+ *    country: Vietnam - VNM
+ * ----------------------------------------------------------
+ *    title: 
+ *    source: 
+ *    submission: 
+ *    status: WIP
+ * ----------------------------------------------------------
+ *    tags: 
+ *    complexity: 
+ *    note: 
+**/
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <utility>
 
 using namespace std;
 
+// --- [ DEBUGGING & LOCAL CONFIG ] ---
+#if __has_include("TomDev.h") && defined(LOCAL)
+    #include "TomDev.h"
+    #define dbg(x,i) cerr << "BreakPoint(" << i << ") -> " << #x << " = " << (x) << '\n'
+#else
+    #define dbg(x,i)
+#endif
+#define NAH_I_WOULD_WIN 0
+
+// --- [ MACROS ] ---
+#define all(x,bonus) (x).begin()+(bonus),(x).end()
+#define range(x,st,ed) (x).begin()+(st),(x).begin()+(ed)+1
+#define filter(x,bonus) (x).erase(unique((x).begin()+(bonus), (x).end()), (x).end())
+#define rall(x,bonus) (x).rbegin(),(x).rend()-(bonus)
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fi first
+#define se second
+#define eb emplace_back
+#define sz(x) (int)(x).size()
+
+// --- [ TYPES & ALIASES ] ---
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pll = pair<long long,long long>;
+using pld = pair<long double,long double>;
+using pii = pair<int,int>;
+using pill = pair<int,long long>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<long long>;
+using vvll = vector<vector<long long>>;
+using vpii = vector<pair<int,int>>;
+using vpill = vector<pair<int,long long>>;
+using vpll = vector<pair<long long,long long>>;
+
+void setup(){
+    if(!fopen("main.INP", "r")) return;
+    freopen("main.INP", "r", stdin);
+    freopen("main.OUT", "w", stdout);
+}
+
+// ----------------------- [ CONFIG & CONSTANTS ] -----------------------
+
+
+// ----------------------- [ FUNCTIONS ] -----------------------
+
+
+// ----------------------- [ MAIN ] -----------------------
 int main(){
-    int a,b;
-    cin >> a >> b;
-    if(a > 200) cout << a + b << '\n';
-    else cout << a + b + 1 << '\n';
+    fastio;
+    setup();
+    
+    int l = 1, r = 1e9;
+    while(l < r){
+        int mid = l + ((r-l+1)>>1);
+        
+        cout << "? " << mid << endl;
+
+        char type;
+        cin >> type;
+
+        if(type == '<'){
+            r = mid-1;
+        }
+        else l = mid;
+    }
+    cout << "! " << l << endl;
+    
+    return NAH_I_WOULD_WIN;
 }
