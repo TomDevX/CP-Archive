c9d2f19633a3e3c8da6ba045d1adadf1ef1ef02e
TomDevX <hoangquan0111@gmail.com>
2026-04-28 22:46:37 +0700

    add VNOJ comnet VOI 13 Bài 3 - Mạng truyền thông

--- Solutions/VNOJ/comnet.cpp
+++ Solutions/VNOJ/comnet.cpp
@@ -1,16 +1,16 @@
 /**
  *    author: TomDev - Tran Hoang Quan
- *    created: 2026-04-28 08:08:39
+ *    created: 2026-04-28 15:55:54
  *    country: Vietnam - VNM
  * ----------------------------------------------------------
- *    title: 
- *    source: 
- *    submission: 
- *    status: WIP
+ *    title: VOI 13 Bài 3 - Mạng truyền thông
+ *    source: https://oj.vnoi.info/problem/comnet
+ *    submission: https://oj.vnoi.info/src/12204687
+ *    status: AC
  * ----------------------------------------------------------
- *    tags: 
- *    complexity: 
- *    note: 
+ *    tags: DSU, MST
+ *    complexity: O(q \cdot m + m \log m + q \cdot s \log s)
+ *    note: On each query, sort the original edges first. Then make a separate array for changed edges, sort them on each Q query. Do Kruskal algo on the edges (remember to prefer the changed edges if met on the original one) with two pointers on 2 arrays. We only union_set with all edges that has w < target_w (in case there are multiple MST) and then check
 **/
 
 #include <iostream>
@@ -19,8 +19,9 @@
 #include <cstdio>
 #include <string>
 #include <utility>
-#include <numeric>
 #include <cstring>
+#include <numeric>
+#include <bitset>
 
 using namespace std;
 
@@ -68,29 +69,50 @@ void setup(){
 
 // ----------------------- [ CONFIG & CONSTANTS ] -----------------------
 const int N = 1e5+2, M = 1e6+2;
-int par[N], sz[N];
-bool changed[M];
 
-pii Es[M];
-vpii E,E2;
+int new_w[M], cur_w[M], par[N], sz[N];
+bitset<M> ischanged;
+int editedEdge[M], edges[M];
+int hisid = 0;
+
+// struct Edge{
+//     int u,v;
+    
+//     Edge(int _u = 0, int _v = 0) : u(_u), v(_v) {};
+// };
+
+// struct DSU_change{
+//     int u,v;
+//     bool sz_changed;
+
+//     DSU_change(int _u = 0, int _v = 0, bool _sz_changed = false) : u(_u), v(_v), sz_changed(_sz_changed) {};
+// };
+
+pii adj[M];
+// DSU_change his[M];
 
 // ----------------------- [ FUNCTIONS ] -----------------------
-void init(int n, int m){
+void init(int n){
     iota(par + 1, par + n + 1, 1);
-    memset(sz, 0, sizeof(int)*(n+1));
-    E.clear();
-    memset(Es,0,sizeof(E[0])*m);
-}   
-
-void rest(int n, int m){
-    memset(changed,0, sizeof(changed[0])*m);
-    iota(par + 1, par + n + 1, 1);
-    memset(sz, 0, sizeof(sz[0])*(n+1));
-    E2.clear();
+    memset(sz,0,sizeof(sz[0])*(n+1));
 }
 
-bool cmp(const pii& a, const pii& b){
-    return a.se < b.se;
+// void rollback(int ver){
+//     while(hisid > ver){
+//         DSU_change last = his[hisid];
+//         hisid--;
+
+//         par[last.v] = last.v;
+//         if(last.sz_changed) sz[last.u]--;
+//     }
+// }
+
+void restQ(int n, int s){
+    init(n);
+    // rollback(0);
+    for(int i = 1; i <= s; i++){
+        ischanged[editedEdge[i]] = 0;
+    }
 }
 
 int find_set(int u){
@@ -98,15 +120,68 @@ int find_set(int u){
     return par[u] = find_set(par[u]);
 }
 
-bool union_set(int a, int b){
+void union_set(int a, int b){
     a = find_set(a), b = find_set(b);
-    if(a == b) return false;
+    if(a == b) return;
 
     if(sz[a] < sz[b]) swap(a,b);
+
+    // his[++hisid] = DSU_change(a,b,(sz[a]==sz[b]));
+
     if(sz[a] == sz[b]) sz[a]++;
     par[b] = a;
+}
 
-    return true;
+
+void solve(int n, int m, int q){
+    for(int i = 1; i <= m; i++){
+        int u,v,w;
+        cin >> u >> v >> w;
+        cur_w[i] = w;
+        adj[i] = {u,v};
+        edges[i] = i;
+    }
+    sort(edges + 1, edges + m + 1, [](const int id1, const int id2) noexcept -> bool {
+        return cur_w[id1] < cur_w[id2];
+    });
+    
+    while(q--){
+        int k,s;
+        cin >> k >> s;
+        int target_w = cur_w[k];
+        
+        for(int i = 1; i <= s; i++){
+            int id, w;
+            cin >> id >> w;
+            new_w[id] = w;
+            editedEdge[i] = id;
+            ischanged[id] = 1;
+        }
+        if(ischanged[k]) target_w = new_w[k];
+        
+        sort(editedEdge + 1, editedEdge + s + 1, [](const int id1, const int id2) noexcept -> bool {
+            return new_w[id1] < new_w[id2];
+        });
+        
+        int l1 = 1, l2 = 1;
+        while(l1 <= m || l2 <= s){
+            int cur;
+            while(l1 <= m && ischanged[edges[l1]]) l1++;
+            if(l1 <= m && (l2 > s || cur_w[edges[l1]] < new_w[editedEdge[l2]])){
+                cur = edges[l1++];
+            }
+            else{
+                cur = editedEdge[l2++];
+            }
+            
+            if((ischanged[cur] ? new_w[cur] : cur_w[cur]) >= target_w) break;
+            union_set(adj[cur].fi, adj[cur].se);
+        }
+        
+        cout << (find_set(adj[k].fi) ==  find_set(adj[k].se) ? "YES" : "NO") << '\n';
+
+        restQ(n,s);
+    }
 }
 
 // ----------------------- [ MAIN ] -----------------------
@@ -116,56 +191,14 @@ int main(){
     
     int tc;
     cin >> tc;
-    E.reserve(M);
-    E2.reserve(101);
-
+    
+    init(1e5);
     while(tc--){
         int n,m,q;
         cin >> n >> m >> q;
 
-        init(n,m);
-
-        for(int i = 1; i <= m; i++){
-            int u,v,w;
-            cin >> u >> v >> w;
-            Es[i] = make_pair(u,v);
-            E.eb(i,w);
-        }   
-
-        sort(all(E,0), cmp);
-
-        while(q--){
-            int k,s;
-            cin >> k >> s;
-            for(int i = 1; i <= s; i++){
-                int id, w;
-                cin >> id >> w;
-                E2.eb(id,w);
-                changed[id] = true;
-            }
-
-            sort(all(E2,0),cmp);
-
-            int l1 = 0, l2 = 0;
-            bool checked = false;
-            while(l1 < sz(E) || l2 < sz(E2)){
-                pii cur;
-                if(l2 == sz(E2) || (l1 < sz(E) && E[l1].se < E2[l2].se)){
-                    if(changed[E[l1].fi]) continue;
-                    cur = E[l1];
-                    l1++;
-                }
-                else{
-                    cur = E2[l2];
-                    l2++;
-                }
-
-                if(Es[])
-            }
-
-            cout << (checked ? "YES" : "NO") << '\n';
-            rest(n,m);
-        }
+        solve(n,m,q);
+        // restTC(n);
     }
     
     return NAH_I_WOULD_WIN;
